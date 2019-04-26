6b7c9915defce5cb1713588abb391272ea4aa8e3
David <34044429+dgowdy@users.noreply.github.com>
2019-04-26 07:10:57 -0400

    Update InstallApps.ps1

--- scripts/InstallApps.ps1
+++ scripts/InstallApps.ps1
@@ -1,3 +1,6 @@
+# Use Remembered Arguments For Upgrade
+choco feature enable -n useRememberedArgumentsForUpgrades
+
 ### HACK Workaround choco / boxstarter path too long error
 ## https://github.com/chocolatey/boxstarter/issues/241
 New-Item -Path "$env:userprofile\AppData\Local\Temp\ChocoCache" -ItemType directory -Force | Out-Null
@@ -8,15 +11,15 @@ $ChocoCachePath = "--cacheLocation=`"$env:userprofile\AppData\Local\Temp\ChocoCa
 cinst -y NetFx3 -source windowsfeatures
 
 #--- Tools ---
-cup -y 7zip.install $ChocoCachePath
-cup -y notepadplusplus.install $ChocoCachePath
-cup -y sysinternals --params="'/InstallDir:C:\Vivid\Sysinternals'" $ChocoCachePath
-cup -y treesizefree $ChocoCachePath
+choco upgrade -y 7zip.install $ChocoCachePath
+choco upgrade -y notepadplusplus.install $ChocoCachePath
+choco upgrade -y sysinternals $ChocoCachePath --params="/InstallDir:C:\Vivid\Sysinternals"
+choco upgrade -y treesizefree $ChocoCachePath
 
 #--- Apps ---
-cup -y adobereader $ChocoCachePath
-cup -y googlechrome $ChocoCachePath
-cup -y putty.install $ChocoCachePath
+choco upgrade -y adobereader $ChocoCachePath
+choco upgrade -y googlechrome $ChocoCachePath
+choco upgrade -y putty.install $ChocoCachePath
 
 # personalize
 #choco install -y office365business
